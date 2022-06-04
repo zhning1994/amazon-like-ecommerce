f5663b23dee98502f5a1e8ec4ef3694fd996269a
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-04 17:56:15 +0800

    initialize how the pages should be rendered

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import createCache from '@emotion/cache'
+import { CacheProvider } from '@emotion/react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createCache({ key: "css" });
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Component {...pageProps} />
+    </CacheProvider>
+  )
 }
 
 export default MyApp
