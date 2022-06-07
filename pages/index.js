a4cd7b6a0bba29300bc5eaed2f218a33af44a705
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-07 17:44:06 +0800

    import Layout to index.js

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,10 @@ import { Typography } from '@mui/material'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Ning Style EShop</title>
-        <meta name="description" content="Ecommerce app using Nextjs and Sanity" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Typography component="h1" variant='h1' >Ning EShop</Typography>
-    </div>
+    <Layout>List </Layout>
   )
 }
