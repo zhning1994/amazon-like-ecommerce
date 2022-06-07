a4c9d42feb80f679a9211a02c09e7a19f0dcda8e
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-07 17:41:34 +0800

    create the layout of the pages via material ui, setting the theme of typography and palette

---
+++ components/Layout.js
@@ -0,0 +1,59 @@
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider, CssBaseline, AppBar, Toolbar, Link, Typography, Container, Box } from "@mui/material";
+import Head from "next/head";
+import NextLink from "next/link";
+import classes from "../utils/classes";
+
+export default function Layout({ title, description, children }) {
+    const theme = createTheme({
+        typography: {
+            h1: {
+                fontSize: '1.6rem',
+                fontWeight: 400,
+                margin: '1rem 0',
+            },
+            h2: {
+                fontSize: '1.4rem',
+                fontWeight: 400,
+                margin: '1rem 0',
+            },
+        },
+        palette: {
+            mode: 'light',
+            primary: {
+                main: '#fb8500'
+            },
+            secondary: {
+                main: '#219ebc'
+            }
+        }
+    });
+    return (
+        <>
+            <Head>
+                <title>
+                    <title>{title ? `${title} - Ning Ecommerce` : 'Ning Ecommerce'}</title>
+                    {description && <meta name="description" content={description}></meta>}
+                </title>
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <AppBar position="static" sx={classes.appbar}>
+                    <Toolbar sx={classes.toolbar}>
+                        <NextLink href="/" passHref>
+                            <Link>
+                                <Typography sx={classes.brand}>Ning Ecommerce</Typography>
+                            </Link>
+                        </NextLink>
+                    </Toolbar>
+                </AppBar>
+                <Container component="main" sx={classes.main}>
+                    {children}
+                </Container>
+                <Box component="footer" sx={classes.footer}>
+                    <Typography>All rights reserved. Ning Ecommerce</Typography>
+                </Box>
+            </ThemeProvider>
+        </>
+    )
+}
