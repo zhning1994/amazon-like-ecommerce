92a3ecc0154d6eb9abcce0d2550b621f01d7769f
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-08 17:31:48 +0800

    fetch data from sanity and render it

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,44 @@
-import { Typography } from '@mui/material'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from "../styles/Home.module.css";
+import { Alert, CircularProgress, Grid, Typography } from '@mui/material'
 import Layout from "../components/Layout";
+import { useEffect, useState } from 'react';
+import client from '../utils/client';
+import ProductItem from '../components/ProductItem';
 
 export default function Home() {
+  const [state, setState] = useState({
+    products: [],
+    error: '',
+    loading: true
+  });
+
+  const { loading, error, products } = state;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const products = await client.fetch(`*[_type == "product"]`);
+        setState({ products, loading: false });
+      } catch (error) {
+        setState({ loading: false, error: error.message });
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <Layout>List </Layout>
+    <Layout> {loading ? (
+      <CircularProgress />
+    ) : error ? (
+      <Alert variant="danger">{error}</Alert>
+    ) : (
+      <Grid container spacing={3}>
+        {products.map((product) => (
+          <Grid item md={4} key={product.slug}>
+            <ProductItem product={product} ></ProductItem>
+          </Grid>
+        ))}
+      </Grid>
+    )}</Layout>
   )
 }
