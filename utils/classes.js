a047830d135e4ebd6b2429c8ea33d74b72f576f2
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-09 23:36:56 +0800

    add section and smallText classes

--- utils/classes.js
+++ utils/classes.js
@@ -1,26 +1,33 @@
 const classes = {
-    main: {
-        marginTop: 2,
-        minHeight: '80vh',
+  section: {
+    marginTop: 1,
+    marginBottom: 1,
+  },
+  smallText: {
+    fontSize: '15px',
+  },
+  main: {
+    marginTop: 2,
+    minHeight: '80vh',
+  },
+  footer: {
+    marginTop: 1,
+    textAlign: 'center',
+  },
+  appbar: {
+    backgroundColor: '#023047',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 1,
     },
-    footer: {
-        marginTop: 1,
-        textAlign: 'center',
-    },
-    appbar: {
-        backgroundColor: '#023047',
-        '& a': {
-            color: '#ffffff',
-            marginLeft: 1,
-        },
-    },
-    toolbar: {
-        justifyContent: 'space-between',
-    },
-    brand: {
-        fontWeight: 'bold',
-        fontSize: '1.5rem',
-    },
-}
+  },
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+  },
+};
 
 export default classes;
