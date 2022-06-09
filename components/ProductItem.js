37a0b9744977264435ed74d7601912448d96fb8b
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-09 23:36:02 +0800

    remove <Typography/> for product rating, and use <Rating/> component instead

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import { Card, CardActionArea, CardActions, CardMedia, CardContent, Typography, Button } from '@mui/material';
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardMedia,
+  CardContent,
+  Typography,
+  Button,
+  Rating,
+} from '@mui/material';
 import NextLink from 'next/link';
 import { urlForThumbnail } from '../utils/image';
 
 export default function ProductItem({ product }) {
-    return (
-        <Card>
-            <NextLink href={`/product/${product.slug.current}`} passHref >
-                <CardActionArea>
-                    <CardMedia component="img" image={urlForThumbnail(product.image)} title={product.name}></CardMedia>
-                    <CardContent>
-                        <Typography>{product.name}</Typography>
-                        <Typography>{product.rating}({product.numReviews} reviews)</Typography>
-                    </CardContent>
-                </CardActionArea>
-            </NextLink>
-            <CardActions>
-                <Typography>RM{product.price}</Typography>
-                <Button size="small" color="primary">
-                    Add to cart
-                </Button>
-            </CardActions>
-        </Card>
-    )
+  return (
+    <Card>
+      <NextLink href={`/product/${product.slug.current}`} passHref>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            image={urlForThumbnail(product.image)}
+            title={product.name}></CardMedia>
+          <CardContent>
+            <Typography>{product.name}</Typography>
+            <Rating value={product.rating} readOnly></Rating>
+          </CardContent>
+        </CardActionArea>
+      </NextLink>
+      <CardActions>
+        <Typography>RM{product.price}</Typography>
+        <Button size="small" color="primary">
+          Add to cart
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
