947640e6ca80c1122d93eb9e273c129f530e0f8e
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-10 17:22:29 +0800

    add StoreProvider on top of <Component/>

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
-import '../styles/globals.css'
-import createCache from '@emotion/cache'
+import '../styles/globals.css';
+import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
+import { StoreProvider } from '../utils/Store';
 
-const clientSideEmotionCache = createCache({ key: "css" });
-function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
+const clientSideEmotionCache = createCache({ key: 'css' });
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}) {
   return (
     <CacheProvider value={emotionCache}>
-      <Component {...pageProps} />
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
     </CacheProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
