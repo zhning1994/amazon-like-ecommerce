850ec5384730b5aa3cf458ff5034fb6cf5ca2a7b
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-13 19:17:34 +0800

    pass addToCartHandler to <ProductItem/> component at home page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-import { Alert, CircularProgress, Grid, Typography } from '@mui/material'
-import Layout from "../components/Layout";
-import { useEffect, useState } from 'react';
+import { Alert, CircularProgress, Grid } from '@mui/material';
+import Layout from '../components/Layout';
+import { useEffect, useState, useContext } from 'react';
 import client from '../utils/client';
 import ProductItem from '../components/ProductItem';
+import { Store } from '../utils/Store';
+import axios from 'axios';
+import { useSnackbar } from 'notistack';
+import { useRouter } from 'next/router';
+import { urlForThumbnail } from '../utils/image';
 
 export default function Home() {
+  const router = useRouter();
+  const {
+    state: { cart },
+    dispatch,
+  } = useContext(Store);
+
+  const { enqueueSnackbar } = useSnackbar();
+
   const [state, setState] = useState({
     products: [],
     error: '',
-    loading: true
+    loading: true,
   });
 
   const { loading, error, products } = state;
@@ -21,24 +34,56 @@ export default function Home() {
       } catch (error) {
         setState({ loading: false, error: error.message });
       }
-    }
+    };
 
     fetchData();
   }, []);
 
+  const addToCartHandler = async (product) => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      enqueueSnackbar('Sorry. Product is out of stock', { variant: 'error' });
+      return;
+    }
+    dispatch({
+      type: 'CART_ADD_ITEM',
+      payload: {
+        _key: product._id,
+        name: product.name,
+        countInStock: product.countInStock,
+        slug: product.slug.current,
+        price: product.price,
+        image: urlForThumbnail(product.image),
+        quantity,
+      },
+    });
+    enqueueSnackbar(`${product.name} added to the cart`, {
+      variant: 'success',
+    });
+
+    router.push('/cart');
+  };
+
   return (
-    <Layout> {loading ? (
-      <CircularProgress />
-    ) : error ? (
-      <Alert variant="danger">{error}</Alert>
-    ) : (
-      <Grid container spacing={3}>
-        {products.map((product) => (
-          <Grid item md={4} key={product.slug}>
-            <ProductItem product={product} ></ProductItem>
-          </Grid>
-        ))}
-      </Grid>
-    )}</Layout>
-  )
+    <Layout>
+      {' '}
+      {loading ? (
+        <CircularProgress />
+      ) : error ? (
+        <Alert variant="danger">{error}</Alert>
+      ) : (
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item md={4} key={product.slug}>
+              <ProductItem
+                product={product}
+                addToCartHandler={addToCartHandler}></ProductItem>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Layout>
+  );
 }
