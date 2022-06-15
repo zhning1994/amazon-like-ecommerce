3f40ea7bf64f101d78dfbf27c026b8d5c6eeb944
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-06-15 19:49:58 +0800

    add navbar menu, dispatch USER_LOGOUT action and remove userInfo and cartItem cookie if user click logout

--- components/Layout.js
+++ components/Layout.js
@@ -10,15 +10,20 @@ import {
   Box,
   Switch,
   Badge,
+  Button,
+  Menu,
+  MenuItem,
 } from '@mui/material';
 import Head from 'next/head';
 import NextLink from 'next/link';
 import classes from '../utils/classes';
 import { Store } from '../utils/Store';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import jsCookie from 'js-cookie';
+import { useRouter } from 'next/router';
 
 export default function Layout({ title, description, children }) {
+  const router = useRouter();
   const { state, dispatch } = useContext(Store);
   const { darkMode, cart, userInfo } = state;
   const theme = createTheme({
@@ -56,6 +61,23 @@ export default function Layout({ title, description, children }) {
     dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
     const newDarkMode = !darkMode;
     jsCookie.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
+  const [anchorEl, setAnchorEl] = useState(null);
+  const loginMenuCloseHandler = (e, redirect) => {
+    setAnchorEl(null);
+    if (redirect) {
+      router.push(redirect);
+    }
+  };
+  const loginClickHandler = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const logoutClickHandler = () => {
+    setAnchorEl(null);
+    dispatch({ type: 'USER_LOGOUT' });
+    jsCookie.remove('userInfo');
+    jsCookie.remove('cartItems');
+    router.push('/');
   };
   return (
     <>
@@ -98,9 +120,33 @@ export default function Layout({ title, description, children }) {
                 </Link>
               </NextLink>
               {userInfo ? (
-                <NextLink href="/profile" passHref>
-                  <Link>{userInfo.name}</Link>
-                </NextLink>
+                <>
+                  <Button
+                    aria-controls="simple-menu"
+                    aria-haspopup="true"
+                    sx={classes.navbarButton}
+                    onClick={loginClickHandler}>
+                    {userInfo.name}
+                  </Button>
+                  <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={loginMenuCloseHandler}>
+                    <MenuItem
+                      onClick={(e) => loginMenuCloseHandler(e, '/profile')}>
+                      Profile
+                    </MenuItem>
+                    <MenuItem
+                      onClick={(e) =>
+                        loginMenuCloseHandler(e, '/order-history')
+                      }>
+                      Order History
+                    </MenuItem>
+                    <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
+                  </Menu>
+                </>
               ) : (
                 <NextLink href="/login" passHref>
                   <Link>Login</Link>
